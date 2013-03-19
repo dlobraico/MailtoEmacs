be7ef7c7eea1a5b67280283b6d144157a83d05cb
D. LoBraico <dj@lobraico.com>
2013-03-19 02:05:40 -0500

    Initial commit; seems to be basically working as needed

--- MailtoEmacs/AppDelegate.m
+++ MailtoEmacs/AppDelegate.m
@@ -12,7 +12,45 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+  }
+
+- (void)applicationWillFinishLaunching:(NSNotification *)aNotification
+{
+    NSAppleEventManager *appleEventManager = [NSAppleEventManager sharedAppleEventManager];
+    [appleEventManager setEventHandler:self
+                           andSelector:@selector(handleGetURLEvent:withReplyEvent:)
+                         forEventClass:kInternetEventClass andEventID:kAEGetURL];
 }
+
+- (void)passURLToEmacsClient:(NSURL *)url {
+    NSString * mailto_string = [url absoluteString];
+    NSLog(@"Received mailto link: %@", mailto_string);
+    
+    NSString * emacs_snippet = [NSString stringWithFormat:@"(browse-url \"%@\")", mailto_string];
+    NSLog(@"Running emacsclient with snippet: %@", emacs_snippet);
+    
+    NSTask *task;
+    task = [[NSTask alloc] init];
+    [task setLaunchPath: @"/usr/local/bin/emacsclient"];
+    
+    NSArray *arguments;
+    arguments = [NSArray arrayWithObjects: @"-n", @"-a", @"''", @"-c", @"-e", emacs_snippet, nil];
+    [task setArguments: arguments];
+    
+    [task launch];
+    
+    while ([task isRunning]) {
+        sleep(1);
+    }
+    
+    exit(0);
+}
+
+- (void)handleGetURLEvent:(NSAppleEventDescriptor *)event withReplyEvent:(NSAppleEventDescriptor *)replyEvent
+{
+    NSURL *url = [NSURL URLWithString:[[event paramDescriptorForKeyword:keyDirectObject] stringValue]];
+    [self passURLToEmacsClient:url];
+}
+
 
 @end
